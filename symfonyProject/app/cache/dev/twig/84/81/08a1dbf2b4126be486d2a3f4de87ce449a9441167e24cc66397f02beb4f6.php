afb63b5cd3a60a60d2ef3758eb10e95e964287ff
rafa.hidalgo <rahife@users.noreply.github.com>
2014-12-30 00:30:41 +0700

    First commit

---
+++ symfonyProject/app/cache/dev/twig/84/81/08a1dbf2b4126be486d2a3f4de87ce449a9441167e24cc66397f02beb4f6.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/index.html.twig */
+class __TwigTemplate_848108a1dbf2b4126be486d2a3f4de87ce449a9441167e24cc66397f02beb4f6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Hello World!!!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
